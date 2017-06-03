acd2b32871c6530ed9ff5d71728de474282d45ec
Baksztag <jakub.a.kret@gmail.com>
2017-06-03 09:39:39 +0200

    Move connection details to .env file

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
+//EXPRESS
 const express = require('express');
 const app = express();
+
+//DATABASE
+const env = require('node-env-file');
+env(__dirname + '/.env');
 const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
 
-const connectionDetails = require('./ConnectionDetails');
-
+//VIEW ENGINE
 app.set('view engine', 'ejs');
 
-
+//START LISTENING
 app.listen(3000, function() {
     console.log('listening on 3000')
 });
 
 
-const connection = connectionDetails.getConnection();
-
-connection.connect();
-
+//ROUTES
 app.get('/student', function(req, res){
     // reqjson = JSON.parse(req);
     // reqjson.get()
     // res.sendFile(__dirname + '/index.html');
     connection.query('SELECT * from Student where IndexNumber = 69', function (error, results, fields) {
-        if (error) throw error;
+        if (error) return console.log(error);
         console.log(results[0].Lastname);
         // ress = '<h1>' + results[0].Firstname + ' ' + results[0].Lastname + ' ' + results[0].Title + '</h1>'
         // res.send(ress);
@@ -37,13 +44,9 @@ app.get('/', function(req, res){
     //
     // });
     res.render('index.ejs', {firstname: ''})
+    // res.send("OK");
 });
 
-
-// connection.query('SELECT Lastname from Student where Lastname = ', function (error, results, fields) {
-//     if (error) throw error;
-//     console.log(results[0].Lastname);
-//     ress = '<h1>' + results[0].Firstname + ' ' + results[0].Lastname + ' ' + results[0].Title + '</h1>'
-//     res.send(ress);
-// });
-// JSON.parse(req);
+app.get('/subject/:id/opinions', function (req, res) {
+    
+});
